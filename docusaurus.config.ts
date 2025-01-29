598cf860e4df5a83d84c26dd0650dd73d116676e
roniAnalytics <pandaterminalteam@gmail.com>
2025-01-30 03:01:29 +0400

    fixed nav

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Deploy Docs',
+      
       logo: {
         alt: 'Deploy Logo',
         src: 'img/logo.svg',
